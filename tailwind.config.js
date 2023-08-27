dabea8a9f956fa0722fff2db612c2506641688bb
Supachai Sakupan <caneliean@gmail.com>
2023-08-27 22:44:51 +0700

    feat:RoiUp Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      Prompt: ["Prompt"],
+      Poppins: ["Poppins"],
+    },
     fontSize: {
       sm: "0.8rem",
       base: "1rem",
